88844f098d062f634bc2a718d8ac7ce2cea65b26
Charles Cusson <charlescusson@gmail.com>
2020-03-10 01:26:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function happyHolidaysTo(name){
 function happyCustomHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(){
+function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`
 }
+
